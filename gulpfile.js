e91dc86872780626adfa43cbf07c006a11ad5eb3
Wellivelton Fonseca <welliveltonfonseca@gmail.com>
2024-04-11 13:53:54 -0300

    adição de compressão do javascripts e mudança de versão do gulp de 5.0.0 para 4.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+
+function comprimeJavascripts() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe.dest('./build/scripts/');
+}
 
 function comprimeImages() {
     return (gupl.src)('./source/images/*')
@@ -16,4 +23,5 @@ function compilaSass() {
 exports.default = function() {
     gulp.watch('./source/images/*', {ignoreInitial: false}, gulp.series(comprimeImages));
     gulp.watch('./source/styles/*.sass', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*js', {ignoreInitial: false}, gulp.series(comprimeJavascripts));
 }
